e009782b82ccb72b6a4ae6f2c2490eb19ceb9391
Dylan Pinn <dylan@arcadiandigital.com.au>
2016-08-11 14:54:13 +1000

    Add MovieCell component

---
+++ MovieCell.js
@@ -0,0 +1,82 @@
+/**
+ * @flow
+ */
+'use strict';
+
+import React, {Component} from 'react';
+import {
+  Image,
+  Platform,
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  TouchableNativeFeedback,
+  View
+} from 'react-native';
+
+var getStyleFromScore = require('./getStyleFromScore');
+var getImageSource = require('./getImageSource');
+var getTextFromScore = require('./getTextFromScore');
+
+class MovieCell extends Component {
+  render() {
+    var criticsScore = this.props.ratings.critics_score;
+    var TouchableElement = TouchableHighlight;
+    if (Platform.OS === 'android') {
+      TouchableElement = TouchableNativeFeedback;
+    }
+    return (
+      <View>
+        <TouchableElement
+          onPress={this.props.onSelect}
+          onShowUnderlay={this.props.onHighlight}
+          onHideUnderlay={this.props.onUnhighlight}
+        >
+          <View style={styles.textContainer}>
+            <Text style={styles.movieTitle} numberofLines={1}>
+              {this.props.movie.year}
+              {' '}&bull;{' '}
+              <Text style={getStyleFromScore(criticsScore)}>
+                Critics {getTextFromScore(criticsScore)}
+              </Text>
+            </Text>
+          </View>
+        </TouchableElement>
+      </View>
+    );
+  }
+}
+var styles = StyleSheet.create({
+  textContainer: {
+    flex: 1,
+  },
+  movieTitle: {
+    flex: 1,
+    fontSize: 16,
+    fontWeight: '500',
+    marginBottom: 2,
+  },
+  movieYear: {
+    color: '#999999',
+    fontSize: 12,
+  },
+  row: {
+    alignItems: 'center',
+    backgroundColor: 'white',
+    flexDirection: 'row',
+    padding: 5,
+  },
+  cellImage: {
+    backgroundColor: '#dddddd',
+    height: 93,
+    marginRight: 10,
+    width: 60,
+  },
+  cellBorder: {
+    backgroundColor: 'rgba(0, 0, 0, 0.1)',
+    height: StyleSheet.hairlineWidth,
+    marginLeft: 4,
+  },
+})
+
+module.exports = MovieCell;
